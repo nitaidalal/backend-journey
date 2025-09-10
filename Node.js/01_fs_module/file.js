804ed0d242c0b30cdb8c070f61c74c8d88f4d62c
Nitai Dalal <dalalnitai7@gmail.com>
2025-09-10 22:21:04 +0530

    Uncommented file system operations in file.js

--- Node.js/01_fs_module/file.js
+++ Node.js/01_fs_module/file.js
@@ -1,75 +1,70 @@
 const fs = require("fs");
 
 //write
-// fs.writeFileSync("./text.txt","This is a sync code");
+fs.writeFileSync("./text.txt","This is a sync code");
 
-// fs.writeFile("./test.txt","This is a async code", (err) => {
-//     console.log(err);
-// })
+fs.writeFile("./test.txt","This is a async code", (err) => {
+    console.log(err);
+})
 
 //read
-// const res = fs.readFileSync("./text.txt","utf-8")
-// console.log(res);
-// fs.readFile("./test.txt","utf-8",(err,res) => {
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log(res)
-//     }
-// })
+const res = fs.readFileSync("./text.txt","utf-8")
+console.log(res);
+fs.readFile("./test.txt","utf-8",(err,res) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log(res)
+    }
+})
 
 
 //update/append
+fs.appendFileSync("./text.txt", new Date().toDateString())
 
-// fs.appendFileSync("./text.txt", new Date().toDateString())
-
-// fs.appendFile("./log.txt",`this is created on ${new Date().toDateString()}\n`,
-// (err,res) => {
-//     if(err){
-//         console.log(err)
-//     }else{
-//         console.log(res)
-//     }
-// })
+fs.appendFile("./log.txt",`this is created on ${new Date().toDateString()}\n`,
+(err,res) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log(res)
+    }
+})
 
 //delete
-// try{
-//     fs.unlinkSync("./nitai.txt");
-//     console.log("file deleted successfully");
-// } catch(err){
-//     console.error("Error: ",err);
-// }
+try{
+    fs.unlinkSync("./nitai.txt");
+    console.log("file deleted successfully");
+} catch(err){
+    console.error("Error: ",err);
+}
 
-// fs.unlink("./hello.txt",
-//     (err) => {
-//         if(err){
-//             console.log(err);
-//         }else{
-//             console.log("deleted successsfully");
-//         }
-//     }
-// )
+fs.unlink("./hello.txt",
+    (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("deleted successsfully");
+        }
+    }
+)
 
 
 //Create folder
-// fs.mkdirSync("parent/child/grandchild", { recursive: true });
-// console.log("Nested folders created successfully");
+fs.mkdirSync("parent/child/grandchild", { recursive: true });
+console.log("Nested folders created successfully");
 
 
 //Get info about file/folder
+const stats = fs.statSync("text.txt");
+console.log("Is file:", stats.isFile());
+console.log("Is directory:", stats.isDirectory());
+console.log("Size:", stats.size, "bytes");
 
-// const stats = fs.statSync("text.txt");
-// console.log("Is file:", stats.isFile());
-// console.log("Is directory:", stats.isDirectory());
-// console.log("Size:", stats.size, "bytes");
 
-//Copy file/folder synchronously
-
-// fs.cpSync("source.txt", "copy.txt");
-// console.log("File copied successfully");
-
+//Copy file synchronously
+fs.cpSync("source.txt", "copy.txt");
+console.log("File copied successfully");
 
 
 
-// const os = require("os");
-// console.log(os.cpus().length)
